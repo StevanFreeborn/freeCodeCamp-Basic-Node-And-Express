1fc1ed928e66adc599a0948bc675cd8db261f929
StevanFreeborn <sfreeborn7@gmail.com>
2022-04-25 18:00:51 -0500

    moved style.css to proper folder;

--- myApp.js
+++ myApp.js
@@ -1,10 +1,4 @@
 var express = require('express');
 var app = express();
 
-// const handler = (req, res) => {
-//     res.send('Hello Express');
-// }
-
-// app.get('/', handler());
-
 console.log('Hello World!')
