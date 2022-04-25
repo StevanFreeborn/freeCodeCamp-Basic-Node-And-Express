6ce1be2f7cb805355cebef124bed3253ee8341df
StevanFreeborn <sfreeborn7@gmail.com>
2022-04-25 17:16:16 -0500

    added gitignore;

--- myApp.js
+++ myApp.js
@@ -1,4 +1,10 @@
 var express = require('express');
 var app = express();
 
+// const handler = (req, res) => {
+//     res.send('Hello Express');
+// }
+
+// app.get('/', handler());
+
 console.log('Hello World!')
