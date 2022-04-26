362b0c994965ad48334ab9eedae99d238a22fc77
StevanFreeborn <sfreeborn7@gmail.com>
2022-04-26 10:03:35 -0500

    Added logger middleware;

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ if (!process.env.DISABLE_XORIGIN) {
 
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+})
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
