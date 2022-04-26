50649db4e0a6bd84983b1d1f4ce02216a2a64745
StevanFreeborn <sfreeborn7@gmail.com>
2022-04-26 10:08:42 -0500

    added now route;

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.use("/public", express.static(__dirname + "/public"));
 app.use((req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`);
   next();
-})
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
-})
+});
 
 app.get('/json', (req, res) => {
   
@@ -45,7 +45,14 @@ app.get('/json', (req, res) => {
   data.message = data.message.toUpperCase();
 
   res.json(data)
-})
+});
+
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.send(req.time);
+});
 
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
