5479c9d772669b7ed8c57387215466a967b16db7
StevanFreeborn <sfreeborn7@gmail.com>
2022-04-26 10:20:36 -0500

    mode code to myApp.js

--- myApp.js
+++ myApp.js
@@ -3,3 +3,45 @@ var app = express();
 
 console.log('Hello World!')
 
+app.use("/public", express.static(__dirname + "/public"));
+
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
+
+// app.get('/', (req, res) => {
+//   res.send('Hello Express');
+// })
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/json', (req, res) => {
+
+    const data = {
+        message: "Hello json"
+    }
+
+    if (process.env.MESSAGE_STYLE != 'uppercase') {
+        return res.json(data);
+    }
+
+    data.message = data.message.toUpperCase();
+
+    res.json(data)
+});
+
+app.get(
+    "/now",
+    (req, res, next) => {
+      req.time = new Date().toString();
+      next();
+    },
+    (req, res) => {
+      res.send({
+        time: req.time
+      });
+    }
+  );
--- server.js
+++ server.js
@@ -17,49 +17,6 @@ if (!process.env.DISABLE_XORIGIN) {
   });
 }
 
-// app.get('/', (req, res) => {
-//   res.send('Hello Express');
-// })
-
-app.use("/public", express.static(__dirname + "/public"));
-
-app.use((req, res, next) => {
-  console.log(`${req.method} ${req.path} - ${req.ip}`);
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
-app.get('/json', (req, res) => {
-  
-  const data = {
-    message: "Hello json"
-  }
-
-  if(process.env.MESSAGE_STYLE != 'uppercase') {
-    return res.json(data);
-  }
-
-  data.message = data.message.toUpperCase();
-
-  res.json(data)
-});
-
-app.get(
-  "/now",
-  (req, res, next) => {
-    req.time = new Date().toString();
-    next();
-  },
-  (req, res) => {
-    res.send({
-      time: req.time
-    });
-  }
-);
-
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
