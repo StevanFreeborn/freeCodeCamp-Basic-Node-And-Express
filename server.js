dcdd819fce0a27cd9ad4918f582e54fa57549e5a
StevanFreeborn <sfreeborn7@gmail.com>
2022-04-26 09:37:29 -0500

    Add /json route;

--- server.js
+++ server.js
@@ -25,6 +25,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 })
 
+app.get('/json', (req, res) => {
+  
+  const data = {
+    message: "Hello json"
+  }
+
+  res.json(data)
+})
+
 var port = process.env.PORT || 3000;
 bGround.setupBackgroundApp(app, myApp, __dirname).listen(port, function(){
   bGround.log('Node is listening on port '+ port + '...')
